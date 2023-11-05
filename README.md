56b4068339cf8be3b1145ebef91891ccd5fed6cb
fitolobo <rodolfolobo@ug.uchile.cl>
2023-11-05 20:06:44 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Introduction to Applied Machine Learning for Audio: Theory and Implementation
+
+
+The contents of the course "Introduction to Applied Machine Learning for Audio: Theory and Implementation" are designed to convey concepts and understand classical machine learning methods with the goal of training models in a supervised or unsupervised manner, capable of classifying different types of audio, whether into single or multiple labels.
+The lecture is tailored for Sound Engineering students at the University of Chile. 
+
+Upon completing the course, the student will:
+
+- Understand general ideas about the use and operation of different machine learning models.
+
+- Recognize and use preprocessing strategies for audio handling.
+
+- Apply these models to audio classification problems by using various strategies and comparing results using different statistical tools and data visualization techniques.
+
+# References
+
+- Introduction to Probability for Data Science, Stanley H. Chan, 2021, Michigan Publishing. ISBN 978-1-60785-747-1
+Bishop, Christopher M., and Nasser M. Nasrabadi. Pattern recognition and machine learning. Vol. 4. No. 4. New York: springer, 2006.
+
+- Python Crash Course: a hands -on project-based introduction to programming. 
+Second Edition. ISBN-13: 978-1593279288
+
+- Deep Learning, Ian Good Fellow, Yoshua Bengio and Aaron Courville.  MIT Press, 2016. ISBN, 0262035618, 9780262035613
+
+- Neural Networks and Learning Machines, Haykin Simon, 2008. Third Edition
+ISBN 10: 0131471392 ISBN 13: 9780131471399.
+
+- PhD Valerio Velardo: https://github.com/musikalkemist/AudioSignalProcessingForML
+
+- Approaching (ALMOST) Any Machine Learning Problem, Abhishek Thakur, 2019.
+
+- Deep Learning with Python, François Chollet, 2021, Second Edition, Manning Shelter Island.
+
+- Python DataScience Handbook, Essential Tools for Working with Data. Jake VanderPlas O’Reilly, 2016, First Edition.
+
+- PhD Professor Andrew NG: https://www.coursera.org/specializations/machine-learning-introduction
+
+- PhD Keun WooChoi: https://github.com/keunwoochoi/dl4mir
+
---
+++ lectures/clase_02/notebooks/class_notebooks/free-spoken-digit-dataset-master/utils/fsdd.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+import os
+from collections import defaultdict
+import scipy.io.wavfile
+import scipy.ndimage
+from PIL import Image
+
+
+class FSDD:
+    """Summary
+
+    Attributes:
+        file_paths (TYPE): Description
+        recording_paths (TYPE): Description
+    """
+
+    def __init__(self, data_dir):
+        """Initializes the FSDD data helper which is used for fetching FSDD data.
+
+        :param data_dir: The directory where the audiodata is located.
+        :return: None
+
+        Args:
+            data_dir (TYPE): Description
+        """
+
+        # A dict containing lists of file paths, where keys are the label and vals.
+        self.recording_paths = defaultdict(list)
+        file_paths = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
+        self.file_paths = file_paths
+
+        for digit in range(0, 10):
+            # fetch all the file paths that start with this digit
+            digit_paths = [os.path.join(data_dir, f) for f in file_paths if f[0] == str(digit)]
+            self.recording_paths[digit] = digit_paths
+
+    @staticmethod
+    def get_spectrograms(data_dir=None):
+        """
+
+        Args:
+            data_dir (string): Path to the directory containing the spectrograms.
+
+        Returns:
+            (spectrograms, labels): a tuple of containing lists of spectrograms images(as numpy arrays) and their corresponding labels as strings
+        """
+        spectrograms = []
+        labels = []
+
+        if data_dir is None:
+            data_dir = os.path.dirname(__file__) + '/../spectrograms'
+            print(data_dir)
+
+        file_paths = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f)) and '.png' in f]
+        file_paths = sorted(file_paths)
+
+        if len(file_paths) == 0:
+            raise Exception('There are no files in the spectrogram directory. Make sure to run the spectrogram.py before calling this function.')
+
+        for file_name in file_paths:
+            file_path = data_dir + '/' + file_name
+            spectrogram = Image.open(file_path)
+            label = file_name[0]
+            yield spectrogram, label, file_path
